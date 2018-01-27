97cc452549fe01dadc1388e314104785c5d19cb1
Sifoo <basic.gongfu@gmail.com>
2018-01-27 06:56:14 +0100

    added edist.cx

--- src/cixl/util.c
+++ src/cixl/util.c
@@ -40,20 +40,26 @@ char *cx_get_dir(const char *in, char *out, size_t len) {
 size_t cx_str_dist(const char *x, const char *y) {
   const size_t x_max = strlen(x)+1, y_max = strlen(y)+1;
   
-  int d[x_max][y_max];
-  memset(d, 0, sizeof(d));
-  for (size_t i=1; i < x_max; i++) { d[i][0] = i; }
-  for (size_t j=1; j < y_max; j++) { d[0][j] = j; }
+  int *d = calloc(x_max*y_max, sizeof(int));
+  
+  size_t get_index(size_t i, size_t j) {
+    return i*y_max + j;
+  }
+  
+  for (size_t i=1; i < x_max; i++) { d[get_index(i, 0)] = i; }
+  for (size_t j=1; j < y_max; j++) { d[get_index(0, j)] = j; }
 
   for (size_t i=1; i < x_max; i++) {
     for (size_t j=1; j < y_max; j++) {
       int sc = (x[i-1] == y[j-1]) ? 0 : 1;
     
-      d[i][j] = cx_min(d[i-1][j]+1,
-		       cx_min(d[i][j-1]+1,
-			      d[i-1][j-1] + sc));
+      d[get_index(i, j)] = cx_min(d[get_index(i-1, j)]+1,
+				  cx_min(d[get_index(i, j-1)]+1,
+					 d[get_index(i-1, j-1)] + sc));
     }
   }
-
-  return d[x_max-1][y_max-1];
+  
+  int res = d[get_index(x_max-1, y_max-1)];
+  free(d);
+  return res;
 }
