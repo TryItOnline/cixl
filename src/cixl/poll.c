9839da85980537428ae06552cdd454d33f30b337
Sifoo <basic.gongfu@gmail.com>
2018-03-10 04:52:36 +0100

    removed file ref

--- src/cixl/poll.c
+++ src/cixl/poll.c
@@ -8,19 +8,16 @@
 
 struct cx_poll_file {
   int fd;
-  struct cx_file *file;
   struct cx_box on_read;
 };
 
-static struct cx_poll_file *file_init(struct cx_poll_file *pf, struct cx_file *f) {
-  pf->fd = f->fd;
-  pf->file = cx_file_ref(f);
+static struct cx_poll_file *file_init(struct cx_poll_file *pf, int fd) {
+  pf->fd = fd;
   return pf;
 }
 
 static void file_deinit(struct cx_poll_file *f, struct pollfd *pfd) {
   if (pfd->events & POLLIN) { cx_box_deinit(&f->on_read); }
-  cx_file_deref(f->file);
 }
 
 struct cx_poll *cx_poll_init(struct cx_poll *p) {
@@ -35,7 +32,7 @@ struct cx_poll *cx_poll_init(struct cx_poll *p) {
 
 struct cx_poll *cx_poll_deinit(struct cx_poll *p) {
   cx_do_set(&p->files, struct cx_poll_file, f) {
-    file_deinit(f, cx_test(cx_set_get(&p->fds, &f->file->fd)));
+    file_deinit(f, cx_test(cx_set_get(&p->fds, &f->fd)));
   }
   
   cx_set_deinit(&p->files);
@@ -51,9 +48,11 @@ bool cx_poll_read(struct cx_poll *p, struct cx_file *f, struct cx_box *a) {
     pfd = cx_test(cx_set_get(&p->fds, &f->fd));
     if (pfd->events & POLLIN) { return false; }
   } else {
-    ok = file_init(cx_set_insert(&p->files, &f->fd), f);
+    ok = file_init(cx_set_insert(&p->files, &f->fd), f->fd);
+
     pfd = cx_test(cx_set_insert(&p->fds, &f->fd));
     pfd->fd = f->fd;
+    pfd->events = 0;
     pfd->events = 0;
   }
 
